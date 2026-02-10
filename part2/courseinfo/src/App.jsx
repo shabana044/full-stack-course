b5eefbe0e2164d109a79dde8d63dd8812b4e466e
shabana <shabanatalks408@gmail.com>
2026-02-10 09:50:50 +0530

    2.5

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,67 +1,32 @@
-const Header = ({ name }) => {
-  return <h2>{name}</h2>
-}
-
-const Part = ({ part }) => {
-  return (
-    <p>
-      {part.name} {part.exercises}
-    </p>
-  )
-}
-
-const Content = ({ parts }) => {
-  return (
-    <div>
-      {parts.map(part => (
-        <Part key={part.id} part={part} />
-      ))}
-    </div>
-  )
-}
-
-const Course = ({ course }) => {
-  const total = course.parts.reduce((sum, part) => sum + part.exercises, 0)
-
-  return (
-    <div>
-      <Header name={course.name} />
-      <Content parts={course.parts} />
-      <p><strong>Total of {total} exercises</strong></p>
-    </div>
-  )
-}
+import Course from "./components/Course";
 
 const App = () => {
-  const courses = [
-    {
-      name: 'Half Stack application development',
-      id: 1,
-      parts: [
-        { name: 'Fundamentals of React', exercises: 10, id: 1 },
-        { name: 'Using props to pass data', exercises: 7, id: 2 },
-        { name: 'State of a component', exercises: 14, id: 3 },
-        { name: 'Redux', exercises: 11, id: 4 }
-      ]
-    },
-    {
-      name: 'Node.js',
-      id: 2,
-      parts: [
-        { name: 'Routing', exercises: 3, id: 1 },
-        { name: 'Middlewares', exercises: 7, id: 2 }
-      ]
-    }
-  ]
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        id: 1,
+        name: "Fundamentals of React",
+        exercises: 10
+      },
+      {
+        id: 2,
+        name: "Using props to pass data",
+        exercises: 7
+      },
+      {
+        id: 3,
+        name: "State of a component",
+        exercises: 14
+      }
+    ]
+  };
 
   return (
     <div>
-      {courses.map(course => (
-        <Course key={course.id} course={course} />
-      ))}
+      <Course course={course} />
     </div>
-  )
-}
+  );
+};
 
-export default App
-
+export default App;

de379fff3f23bf2b2080e7e4835e7583d0de648e
shabana <shabanatalks408@gmail.com>
2026-02-10 09:54:58 +0530

    2.6

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,35 +1,53 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+
+  const [newName, setNewName] = useState('')
+
+  const handleSubmit = (event) => {
+    event.preventDefault()        // prevent page refresh
+
+    const newPerson = { name: newName }
+
+    setPersons(persons.concat(newPerson)) // add name to list
+    setNewName('') // clear input box
+  }
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h2>Phonebook</h2>
+
+      <form onSubmit={handleSubmit}>
+        <div>
+          name: 
+          <input 
+            value={newName} 
+            onChange={handleNameChange} 
+          />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+
+      {/* Debug line (optional) */}
+      {/* <div>debug: {newName}</div> */}
+
+      <h2>Numbers</h2>
+
+      {persons.map(person => 
+        <p key={person.name}>{person.name}</p>
+      )}
+    </div>
   )
 }
 
 export default App
+

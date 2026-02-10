ba89086c9733842ee1cc130cf20cacd46f7aaf27
shabana <shabanatalks408@gmail.com>
2026-02-10 09:29:18 +0530

    2.1 done

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -14,21 +14,25 @@ const StatisticLine = (props) => {
 }
 
 const Statistics = (props) => {
-  const good = props.good
-  const neutral = props.neutral
-  const bad = props.bad
-  const total = good + neutral + bad
+  const { veryGood, good, neutral, bad } = props
+  const total = veryGood + good + neutral + bad
 
   if (total === 0) {
     return <p>No feedback given</p>
   }
 
-  const average = (good - bad) / total
-  const positive = (good / total) * 100
+  // scoring:
+  // very good = +2
+  // good = +1
+  // neutral = 0
+  // bad = -1
+  const average = (veryGood * 2 + good * 1 + bad * -1) / total
+  const positive = ((veryGood + good) / total) * 100
 
   return (
     <table>
       <tbody>
+        <StatisticLine text="very good" value={veryGood} />
         <StatisticLine text="good" value={good} />
         <StatisticLine text="neutral" value={neutral} />
         <StatisticLine text="bad" value={bad} />
@@ -41,6 +45,7 @@ const Statistics = (props) => {
 }
 
 const App = () => {
+  const [veryGood, setVeryGood] = useState(0)
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
@@ -48,12 +53,18 @@ const App = () => {
   return (
     <div>
       <h1>give feedback</h1>
+      <Button handleClick={() => setVeryGood(veryGood + 1)} text="very good" />
       <Button handleClick={() => setGood(good + 1)} text="good" />
       <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
       <Button handleClick={() => setBad(bad + 1)} text="bad" />
 
       <h1>statistics</h1>
-      <Statistics good={good} neutral={neutral} bad={bad} />
+      <Statistics
+        veryGood={veryGood}
+        good={good}
+        neutral={neutral}
+        bad={bad}
+      />
     </div>
   )
 }

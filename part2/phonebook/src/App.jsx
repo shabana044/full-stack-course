26119d1371d7caf8fbb42e612a3eabd8a6219d97
shabana <shabanatalks408@gmail.com>
2026-02-10 11:14:05 +0530

    2.11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,15 +1,22 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filter, setFilter] = useState('')
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+  }, [])
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -32,7 +39,6 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
-
       <Filter value={filter} onChange={e => setFilter(e.target.value)} />
 
       <h3>Add a new</h3>
@@ -51,4 +57,5 @@ const App = () => {
 }
 
 export default App
+
 
